6026a340bd5f75a617191ac0f3c1945a5fd4378c
Nonfungiblemoyo <65586162+lekte@users.noreply.github.com>
2024-12-27 14:59:13 +0000

    Delete Tests/OpenAIRealtimeAPITests.swift

--- Tests/OpenAIRealtimeAPITests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-@testable import OpenAIRealtimeAPI
-
-final class OpenAIRealtimeAPITests: XCTestCase {
-    func testInitialization() {
-        let sdk = OpenAIRealtimeAPI(modelId: "test-model", ephemeralToken: "test-token")
-        XCTAssertNotNil(sdk)
-    }
-}
